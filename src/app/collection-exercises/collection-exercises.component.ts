db2ea8f38dc630f3cb2c0049570e79972d25aea9
Shane Edwards <shane.edwards9@gmail.com>
2017-04-26 10:11:01 +0100

    Styles compiling with new angular cli compiler

--- src/app/collection-exercises/collection-exercises.component.ts
+++ src/app/collection-exercises/collection-exercises.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     moduleId: module.id,
-    //styleUrls: ['collection-exercises.component.css'],
+
+    styleUrls: ['collection-exercises.component.scss'],
     templateUrl: 'collection-exercises.component.html'
 })
 export class CollectionExercises {}
--- src/app/secure-messages/secure-messages.component.ts
+++ src/app/secure-messages/secure-messages.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     moduleId: module.id,
 
-    //styleUrls: ['secure-messages.component.css'],
+    styleUrls: ['secure-messages.component.scss'],
     templateUrl: 'secure-messages.component.html'
 })
 export class SecureMessages {}
