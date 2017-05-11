920de9e28670bc1fa7f214364db50768638f732d
Duncan Nixon <duncannixon@me.com>
2017-05-11 16:52:25 +0100

    Fixed compile error.

--- src/app/collection-instruments/collection-instrument.actions.ts
+++ src/app/collection-instruments/collection-instrument.actions.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { NgRedux } from '@angular-redux/store';
 
-import { Survey } from './shared/survey.model';
-import { SurveysService } from './surveys.service';
+import { CollectionInstrument } from './shared/collection-instrument.model';
+import { CollectionInstrumentsService } from './collection-instruments.service';
 
 @Injectable()
 export class SurveysActions {
@@ -14,32 +14,32 @@ export class SurveysActions {
 
     constructor(
         private ngRedux: NgRedux<any>,
-        private surveysService: SurveysService) { }
+        private surveysService: CollectionInstrumentsService) { }
 
-    public retrieveSurvey(id: string) {
-
-        this.ngRedux.dispatch({
-            type: SurveysActions.RETRIEVE_SINGLE,
-            id: id
-        });
-
-        const observable = this.surveysService.getSurvey(id);
-
-        observable.subscribe(
-            // Normalise data first to keep entities in data store dry before saving
-            // Update data store
-            (survey: Survey) => {
-                this.receivedSurvey(survey);
-            }
-        );
-        return observable;
-    }
-
-    public receivedSurvey(survey: Survey) {
-
-        this.ngRedux.dispatch({
-            type: SurveysActions.RECEIVED_SINGLE,
-            survey: survey
-        });
-    }
+    // public retrieveSurvey(id: string) {
+    //
+    //     this.ngRedux.dispatch({
+    //         type: SurveysActions.RETRIEVE_SINGLE,
+    //         id: id
+    //     });
+    //
+    //     const observable = this.surveysService.getSurvey(id);
+    //
+    //     observable.subscribe(
+    //         // Normalise data first to keep entities in data store dry before saving
+    //         // Update data store
+    //         (survey: Survey) => {
+    //             this.receivedSurvey(survey);
+    //         }
+    //     );
+    //     return observable;
+    // }
+    //
+    // public receivedSurvey(survey: Survey) {
+    //
+    //     this.ngRedux.dispatch({
+    //         type: SurveysActions.RECEIVED_SINGLE,
+    //         survey: survey
+    //     });
+    // }
 }
