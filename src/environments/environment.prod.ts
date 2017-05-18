226b87f2a259ddd3363487f34d0bd61192295430
Duncan Nixon <duncannixon@me.com>
2017-05-18 10:07:08 +0100

    Fixed prod endpoint urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
     production: false,
     endpoints: {
         // CloudFoundry config
-        collectionExercise: 'http://ras-collection-exercise-demo.apps.mvp.onsclofo.uk/collection-exercise-api/1.0.0/',
-        collectionInstrument: 'http://ras-collection-instrument-demo.apps.mvp.onsclofo.uk/collection-instrument-api/1.0.2/',
-        survey: 'http://localhost:8000/api/'
+        collectionExercise: 'https://ras-collection-exercise-demo.apps.mvp.onsclofo.uk/collection-exercise-api/1.0.0/',
+        collectionInstrument: 'https://ras-collection-instrument-demo.apps.mvp.onsclofo.uk/collection-instrument-api/1.0.2/',
+        survey: 'https://localhost:8000/api/'
     }
 };
