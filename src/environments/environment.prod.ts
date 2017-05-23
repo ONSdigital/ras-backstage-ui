8bdef57bf610828cb02b292f2196999a78a4b37e
Duncan Nixon <duncannixon@me.com>
2017-05-23 14:02:57 +0100

    Added secure message endpoint url to fix the build.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,7 @@ export const environment = {
         // CloudFoundry config
         collectionExercise: 'https://ras-collection-exercise-demo.apps.mvp.onsclofo.uk/collection-exercise-api/1.0.0/',
         collectionInstrument: 'https://ras-collection-instrument-demo.apps.mvp.onsclofo.uk/collection-instrument-api/1.0.2/',
-        survey: 'https://localhost:8000/api/'
+        survey: 'https://localhost:8000/api/',
+        secureMessages: 'http://0.0.0.0:5050/'
     }
 };
