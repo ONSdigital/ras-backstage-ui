15c38e51ec23e5dd8a80823be0333e69fd0f5f0e
Shane Edwards <shane.edwards9@gmail.com>
2017-06-19 17:10:51 +0100

    Changed date sent formatting for viewing a message

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,6 +5,6 @@ export const environment = {
         collectionExercise: 'https://ras-collection-exercise-demo.apps.mvp.onsclofo.uk/collection-exercise-api/1.0.0/',
         collectionInstrument: 'https://ras-collection-instrument-demo.apps.mvp.onsclofo.uk/collection-instrument-api/1.0.2/',
         survey: 'https://localhost:8000/api/',
-        secureMessages: 'http://0.0.0.0:5050/'
+        secureMessages: 'http://sm.apps.mvp.onsclofo.uk/'
     }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -23,6 +23,7 @@ export const environment = {
         collectionExercise: 'http://ras-collection-exercise-demo.apps.mvp.onsclofo.uk/collection-exercise-api/1.0.0/',
         collectionInstrument: 'http://ras-collection-instrument-demo.apps.mvp.onsclofo.uk/collection-instrument-api/1.0.2/',
         // survey: 'http://localhost:8000/api/'
+        // secureMessages: 'http://sm.apps.mvp.onsclofo.uk/',
 
         // TODO remove this
         // collectionExercise: 'http://localhost:8000/api/',
