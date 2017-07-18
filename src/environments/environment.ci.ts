aad567850b2ecd42acd2bfe7bd896cc82c232f64
Shane Edwards <shane.edwards9@gmail.com>
2017-07-18 11:49:25 +0100

    Added new ci environment configuration file

---
+++ src/environments/environment.ci.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: false,
+    endpoints: {
+        collectionExercise: '//ras-api-gateway-ci.apps.devtest.onsclofo.uk/collection-exercise-api/1.0.0/',
+        collectionInstrument: '//ras-api-gateway-ci.apps.devtest.onsclofo.uk/collection-instrument-api/1.0.2/',
+        survey: '//ras-api-gateway-ci.apps.devtest.onsclofo.uk/',
+        secureMessages: '//ras-api-gateway-ci.apps.devtest.onsclofo.uk/'
+    }
+};
