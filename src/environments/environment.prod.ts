b3f1095716f40bf628e7f956dbea051b44df6877
Shane Edwards <shane.edwards9@gmail.com>
2017-08-04 15:42:57 +0100

    Corrected prod and preprod environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
     production: false,
     endpoints: {
-        collectionExercise: '//ras-api-gateway-prod.apps.devtest.onsclofo.uk/',
-        collectionInstrument: '//ras-api-gateway-prod.apps.devtest.onsclofo.uk/collection-instrument-api/1.0.2/',
-        survey: '//ras-api-gateway-prod.apps.devtest.onsclofo.uk/',
-        secureMessages: '//ras-api-gateway-prod.apps.devtest.onsclofo.uk/'
+        collectionExercise: '//ras-api-gateway-prod.apps.prod.cf5.onsclofo.uk/',
+        collectionInstrument: '//ras-api-gateway-prod.apps.prod.cf5.onsclofo.uk/collection-instrument-api/1.0.2/',
+        survey: '//ras-api-gateway-prod.apps.prod.cf5.onsclofo.uk/',
+        secureMessages: '//ras-api-gateway-prod.apps.prod.cf5.onsclofo.uk/'
     }
 };
