fd0d3fa44f85ee4d1050bd67ca43a777ba4d6f9f
Shane Edwards <shane.edwards9@gmail.com>
2017-08-04 15:21:53 +0100

    Added new environment config files

---
+++ src/environments/environment.preprod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: false,
+    endpoints: {
+        collectionExercise: '//ras-api-gateway-preprod.apps.devtest.onsclofo.uk/',
+        collectionInstrument: '//ras-api-gateway-preprod.apps.devtest.onsclofo.uk/collection-instrument-api/1.0.2/',
+        survey: '//ras-api-gateway-preprod.apps.devtest.onsclofo.uk/',
+        secureMessages: '//ras-api-gateway-preprod.apps.devtest.onsclofo.uk/'
+    }
+};
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,9 @@
 export const environment = {
     production: false,
     endpoints: {
-        // CloudFoundry config
-        collectionExercise: 'https://ras-collection-exercise-demo.apps.mvp.onsclofo.uk/collection-exercise-api/1.0.0/',
-        collectionInstrument: 'https://ras-collection-instrument-demo.apps.mvp.onsclofo.uk/collection-instrument-api/1.0.2/',
-        survey: 'https://localhost:8000/api/',
-        secureMessages: 'http://sm.apps.mvp.onsclofo.uk/'
+        collectionExercise: '//ras-api-gateway-prod.apps.devtest.onsclofo.uk/',
+        collectionInstrument: '//ras-api-gateway-prod.apps.devtest.onsclofo.uk/collection-instrument-api/1.0.2/',
+        survey: '//ras-api-gateway-prod.apps.devtest.onsclofo.uk/',
+        secureMessages: '//ras-api-gateway-prod.apps.devtest.onsclofo.uk/'
     }
 };
