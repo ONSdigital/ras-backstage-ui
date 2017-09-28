abefed5bc917b0bfacdf88102695fd88134ca99b
Shane Edwards <shane.edwards9@gmail.com>
2017-09-28 14:47:47 +0100

    Added karma-remap-istanbul for code coverage

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,8 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma')
+      require('@angular/cli/plugins/karma'),
+      require('karma-remap-istanbul')
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -32,8 +33,14 @@ module.exports = function (config) {
       environment: 'dev'
     },
     reporters: config.angularCli && config.angularCli.codeCoverage
-              ? ['progress', 'coverage-istanbul']
+              ? ['progress', 'coverage-istanbul', 'karma-remap-istanbul']
               : ['progress', 'kjhtml'],
+    remapIstanbulReporter: {
+      reports: {
+        lcovonly: 'coverage/lcov.info',
+        html: 'coverage'
+      }
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
